986f693b3f1f146cef6238f91728f0da26c8b188
Conreaux <conreaux.gr@gmail.com>
2021-04-22 11:45:24 +0200

    Correction de bug

--- controllers/restaurantcontroller.js
+++ controllers/restaurantcontroller.js
@@ -2,10 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  Documenu.Restaurants.getByState('NY')
-    .then(result => {
-      res.json(result)
-    });
 });
 
 module.exports = router;
