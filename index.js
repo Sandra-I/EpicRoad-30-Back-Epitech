b11ba64dabc7be0eb42aeeee963d98841e7ba613
Conreaux <conreaux.gr@gmail.com>
2021-05-14 12:12:21 +0200

    get restaurants

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-require("dotenv").config();
-
+require('dotenv').config()
 const express = require("express");
 const app = express();
 var cors = require("cors");
-var sequelize = require("./database");
 
 // Server settings
 app.use(cors());
 app.use(express.json());
 
 // Contollers
-var authcontroller = require("./controllers/authcontroller");
-var usercontroller = require("./controllers/usercontroller");
 var restaurantcontroller = require("./controllers/restaurantcontroller")
 
 // Routes
 app.get("/", (req, res) => res.send("ok"))
-app.use("/api/auth", authcontroller);
-app.use("/api/users", usercontroller);
 app.use("/api/restaurants", restaurantcontroller);
 
 // Start server on port 7000
