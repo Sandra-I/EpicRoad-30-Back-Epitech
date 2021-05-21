5cc1582101c19610ac91ed8d506404fdaf806ab3
Conreaux <conreaux.gr@gmail.com>
2021-05-21 15:40:11 +0200

    get hotel by id

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -8,9 +8,9 @@ const launchPage = async () => {
     let browser = await launchBrowser()
     page = await browser.newPage();
 };
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
 
-    let city = req.body.city;
+    let city = req.query.city;
     if (!city) {
         return res.status(500).send("City not valid")
     }
@@ -28,18 +28,49 @@ router.post("/", async (req, res) => {
                 title: hotelHTML.querySelector('[class="result-title"] span') ? hotelHTML.querySelector('[class="result-title"] span').innerText : "",
                 icon: hotelHTML.querySelector('[class="aspect  is-shown-at-desktop"] > div') ? hotelHTML.querySelector('[class="aspect  is-shown-at-desktop"] > div').style.backgroundImage.slice(4, -1).replace(/"/g, "") : "",
                 address: hotelHTML.querySelector('[class="address-text"]') ? hotelHTML.querySelector('[class="address-text"]').innerText : "",
-                opinions: {
-                    number: hotelHTML.querySelector('[class="review_count"]') ? hotelHTML.querySelector('[class="review_count"]').innerText : "",
-                    url: hotelHTML.querySelector('[class="review_count"]') ? hotelHTML.querySelector('[class="review_count"]').href : ""
-                },
+                opinionsNuber: hotelHTML.querySelector('[class="review_count"]') ? hotelHTML.querySelector('[class="review_count"]').innerText.replace("avis","").replace(/\s/g, '') : "",
                 grade: `${hotelHTML.querySelector('[class="prw_rup prw_common_responsive_rating_and_review_count"] > span') ? parseInt((hotelHTML.querySelector('[class="prw_rup prw_common_responsive_rating_and_review_count"] > span').className.slice(-2)) * 2 / 10) : ""}/10`
             }
         })
     });
 
-    results = results.filter(hotel => hotel.title != "" && hotel.id != "" && hotel.address.toLowerCase().includes(`, ${req.body.city.toLowerCase()}`))
+    results = results.filter(hotel => hotel.title != "" && hotel.id != "" && hotel.address.toLowerCase().includes(`, ${req.query.city.toLowerCase()}`))
 
     res.json(results)
 });
+
+
+router.get("/:id", async (req, res) => {
+
+    let id = req.params.id
+    if (!id) {
+        return res.status(500).send("Id not valid")
+    }
+
+    await launchPage();
+    await page.goto(`https://www.tripadvisor.fr/${id}`);
+
+    // Get the "viewport" of the page, as reported by the page.
+    let results = await page.evaluate(() => {
+        return {
+            title: document.querySelector('#HEADING') ? document.querySelector('#HEADING').innerText : "",
+            address: document.querySelector('[class="_1EqMnQsE _1YIGmrPj jke2_wbp"]') ? document.querySelector('[class="_1EqMnQsE _1YIGmrPj jke2_wbp"]').innerText : "",
+            images: document.querySelectorAll('[class="_3w-DJK-r"] img') ? [...document.querySelectorAll('[class="_3w-DJK-r"] img')].map(img => img.src) : [],
+            phone: document.querySelector('a[href*="tel:"]') ? document.querySelector('a[href*="tel:"]').innerText : "",
+            site: document.querySelector('[data-blcontact*="URL_HOTEL"] a') ? document.querySelector('[data-blcontact*="URL_HOTEL"] a').href : "",
+            description: document.querySelector('[class="_2f_ruteS _1bona3Pu _2-hMril5 _2uD5bLZZ"] [class="cPQsENeY"]') ? document.querySelector('[class="_2f_ruteS _1bona3Pu _2-hMril5 _2uD5bLZZ"] [class="cPQsENeY"]').innerText : "",
+            caracteristics: [...document.querySelectorAll('[class="_1mJdgpMJ"]')].filter(e => e.nextSibling.className.includes("_1nAmDotd")).map(e => {return {label: e.innerText, feature: [...e.nextSibling.querySelectorAll('[class="_2rdvbNSg"]')].map(f => f.innerText)}}),
+            grades: [...document.querySelectorAll('[class*="_318JyS8B"] [class*="_1krg1t5y"]')].map(e => { return { label: e.querySelector('[class="_1h7NKZWM"]').innerText, grade: `${parseInt(e.querySelector('[class*="ui_bubble_rating bubble_"]').className.slice(-2)) * 2 / 10}/10` } }),
+            opinions: {
+                number: document.querySelector('[class="_15eFvJyR _3nlVsadw"] [class="_33O9dg0j"]') ? document.querySelector('[class="_15eFvJyR _3nlVsadw"] [class="_33O9dg0j"]').innerText.replace("avis","").replace(/\s/g, '') : "",
+                ranges: [...document.querySelectorAll('[class="ui_column is-5 is-12-mobile"] [class="ui_checkbox _3gEj_Jb5"]')].map(e => { return {label: e.querySelector('label').innerText, number: e.querySelector('[class="_3fVK8yi6"]').innerText.replace(/\s/g, '')}}),
+                lastOpinions: [...document.querySelectorAll('[class="_2wrUUKlw _3hFEdNs8"]')].map(e => { return {grade: `${parseInt(e.querySelector('[class*="ui_bubble_rating bubble"]').className.slice(-2)) * 2 / 10}/10`, title: e.querySelector('[class="glasR4aX"]').innerText, description: e.querySelector('[class="IRsGHoPm"]').innerText}})
+            }
+        }
+    });
+
+    res.json(results)
+});
+
 
 module.exports = router;
