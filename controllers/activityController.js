83e07aa24b5fc5ff15e83b5a513860d44c7f1d4f
Godefroy13 <godefroy.chabanas@epitech.eu>
2021-07-11 13:19:58 +0200

    Update route

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -13,41 +13,17 @@ router.get("/", (req, res) => {
 
     let params = req.query;
 
-    if (params.adress != ''){
-      //conversion en coordonnées puis requete pour avoir les activités alentour
-      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${params.adress}&key=AIzaSyAxU_TJgCawpDu7wSeaByWDpgVdvIGSiWw`)
-          .then(response => response.json())
-          .then(response => {
-            return Promise.resolve(response.results[0].geometry.location)
-          })
-          .then(function(response){
-            amadeus.shopping.activities.get(
-                {
-                  latitude: response.lat,
-                  longitude: response.lng,
-                  radius: params.radius
-                }
-            ).then(function(resp){
-              res.send(resp.data);
-            }).catch(function(respError){
-              res.send(respError);
-            });
-          })
-      }
-      else{
-        amadeus.shopping.activities.get(
-            {
-              latitude: params.lat,
-              longitude: params.lng,
-              radius: params.radius
-            }
-        ).then(function(resp){
-          res.send(resp.data);
-        }).catch(function(respError){
-          res.send(respError);
-        });
-      }
-
+      amadeus.shopping.activities.get(
+          {
+            latitude: params.lat,
+            longitude: params.lng,
+            radius: params.radius
+          }
+      ).then(function(resp){
+        res.send(resp.data);
+      }).catch(function(respError){
+        res.send(respError);
+      });
 });
 
 router.get("/:id", (req, res) => {
